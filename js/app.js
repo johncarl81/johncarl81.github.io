8d876fd4a976acae8c52395a3975ece904b00a3f
John Ericksen <johncarl81@gmail.com>
2019-01-13 15:09:13 -0700

    Added flickr fetching

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-$(document).foundation()
+$(document).foundation();
+
+$(document).ready(new function () {
+    const apikey = "fcd46d46c64cbdb47ccd4beea09d944b";
+    const userid = "johncarl81";
+    const flickrUrl = "https://api.flickr.com/services/rest/?" +
+        "method=flickr.photos.search" +
+        "&api_key=" + apikey +
+        "&user_id=" + userid +
+        "&format=json" +
+        "&nojsoncallback=1" +
+        "&per_page=24";
+    $.getJSON({
+        url: flickrUrl,
+        success: function(response) {
+            const gallery = document.createDocumentFragment();
+            response.photos.photo.forEach(function(image) {
+                const thumbnailUrl = "https://farm" + image.farm + ".staticflickr.com/" +
+                    image.server + "/" +
+                    image.id + "_" + image.secret + ".jpg";
+
+                const imageUrl = "https://www.flickr.com/photos/" + userid + "/" + image.id;
+
+                // <div class="cell small-6 medium-4 large-2">
+                //   <a href="imageUrl">
+                //     <img class="thumbnail" src="thumbnailUrl">
+                //   </a>
+                // </div>
+
+                const imageDiv = document.createElement("div");
+                imageDiv.classList.add("cell");
+                imageDiv.classList.add("small-6");
+                imageDiv.classList.add("medium-4");
+                imageDiv.classList.add("large-2");
+
+                const imageLink = document.createElement("a");
+                imageLink.href = imageUrl;
+                imageDiv.appendChild(imageLink);
+
+                const imageImg = document.createElement("img");
+                imageImg.className = "thumbnail";
+                imageImg.setAttribute("src", thumbnailUrl);
+                imageLink.appendChild(imageImg);
+
+                gallery.appendChild(imageDiv);
+            });
+
+            document.getElementById("gallery").appendChild(gallery);
+        },
+        error: function(e) {
+            console.error("Error fetching images ", e);
+        }});
+});
